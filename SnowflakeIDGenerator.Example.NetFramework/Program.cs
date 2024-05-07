c66d82efd5d03c9cae0eb46d661ffec00eaf96af
Federico Seckel <fenase@outlook.com>
2024-05-07 20:42:47 -0300

    Added net framework 4.8 example of dependency injection using autofac

---
+++ SnowflakeIDGenerator.Example.NetFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System;
+using System.Globalization;
+
+namespace SnowflakeID.Example.NetFramework
+{
+    internal static class Program
+    {
+        private static IContainer CompositionRoot()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SnowflakeIDGenerator>()
+                .As<ISnowflakeIDGenerator>()
+                .UsingConstructor(typeof(int), typeof(DateTime))
+                .WithParameter("machineId", 22)
+                .WithParameter("customEpoch", DateTime.Parse("2024-05-07", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+            builder.RegisterType<Application>();
+            return builder.Build();
+        }
+
+        static void Main(string[] args)
+        {
+            CompositionRoot().Resolve<Application>().Run();
+        }
+    }
+}
